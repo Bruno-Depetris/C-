1e8d04fcf658a6ea9b0870470ef7dbd6b5427993
BrunoDepetris <80788890+Bruno-Depetris@users.noreply.github.com>
2024-03-27 22:39:33 +0100

    Add files via upload

---
+++ Ejercicio_6/program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejercicio_6{
+    internal class Program{
+        static void Main(string[] args){
+            
+            string[] Dias_Semana =  {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes"};
+            int Horas_trabajadas, Horas_Semanales, Pago_hora, sueldo_millonario;
+            Horas_Semanales = 0;
+            Pago_hora = 0;
+
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\t\t*******************Por favor complete los siguentes datos*******************\t\t");
+            Console.ResetColor();
+            Console.WriteLine("Ingrese las horas trabajadas hoy:");
+
+            for(int i = 0; i <= 4; i++){
+                Console.Write("Dia " + Dias_Semana[i] + ": " );
+                Horas_trabajadas = int.Parse(Console.ReadLine());
+                Horas_Semanales = Horas_Semanales + Horas_trabajadas;
+            } 
+
+            if(Horas_Semanales <= 40){
+                Pago_hora = 150;
+            }
+            if(Horas_Semanales > 40){
+                Pago_hora = 250;
+            }
+
+            sueldo_millonario = Pago_hora * Horas_Semanales;
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("El sueldo es de: $" + sueldo_millonario);
+            Console.ResetColor();
+
+
+
+        }
+    }
+}
---
+++ Ejercicios_1_2_3_4_5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejercicio_1{
+    internal class Program{
+        static void Main(string[] args){
+
+            int numero_uno, numero_dos, multiplicacion;
+
+            Console.WriteLine("Ingrese dos valores por favor"); //writeline es para escribir sobre una una LINEA, mientras que solo write ocupa solo el texto
+            Console.Write(": ");
+            numero_uno = int.Parse(Console.ReadLine());      //int.parse se utiliza para escribir de cadena a numero entero
+            Console.Write(": ");
+            numero_dos = int.Parse(Console.ReadLine());
+
+
+            multiplicacion = numero_uno * numero_dos;    
+            Console.WriteLine("El resultado de la multiplicacion es: " + multiplicacion);
+//POSITIVO O NEGATIVO
+            if(numero_dos  > 0 && numero_uno > 0){
+                Console.WriteLine("Ambos numeros son positivos");
+            }
+            if(numero_dos  < 0 && numero_uno < 0){
+                Console.WriteLine("Ambos numeros son Negativos");
+            }
+            if(numero_dos  < 0 && numero_uno > 0){
+                Console.WriteLine("El segundo numero es negativo y el otro postivo");
+            }
+            if(numero_dos  > 0 && numero_uno < 0){
+                Console.WriteLine("El primer numero es negativo y el otro postivo");
+            }
+//PAR IMPAR
+            if(numero_dos % 2 == 0 && numero_uno % 2 == 0){
+                Console.WriteLine("Ambos numeros son PAR");
+            }
+            if(numero_dos % 2 != 0 && numero_uno % 2 != 0){
+                Console.WriteLine("Ambos numeros son IMPAR");
+            }
+            if(numero_dos % 2 == 0 && numero_uno % 2 != 0){
+                Console.WriteLine("El segundo numero es PAR y el otro IMPAR");
+            }
+            if(numero_uno % 2 == 0 && numero_dos % 2 != 0){
+                Console.WriteLine("El primer numero es PAR y el otro IMPAR");
+            }
+//MAYOR-MENOR
+            if(numero_uno > numero_dos){
+                Console.WriteLine("El numero uno es mayor");
+            }else{
+                Console.WriteLine("El numero dos es menor");
+            }
+        }
+    }
+}
